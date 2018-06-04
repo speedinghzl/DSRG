c5c897a164551fd78ccc9c11753801dfab82f802
Zilong Huang <843133702@qq.com>
2018-06-04 11:37:13 +0800

    Update run-s.sh

--- training/experiment/seed_mc/run-s.sh
+++ training/experiment/seed_mc/run-s.sh
@@ -1,12 +1,14 @@
-# train step 1
-python ../../tools/train.py --solver solver-s.prototxt --weights ../../vgg16_20M_mc.caffemodel --gpu 0
-python ../../tools/test-ms.py --model models/model-s_iter_8000.caffemodel --images list/train_aug_id.txt --dir /workspace/hzl/data/VOCdevkit/voc12 --output /workspace/hzl/data/VOCdevkit/voc12/DSRGOutput --gpu 0 --smooth true
-#python ../../tools/evaluate.py --pred /workspace/hzl/data/VOCdevkit/voc12/DSRGOutput --gt /workspace/hzl/data/VOCdevkit/voc12/SegmentationClass --test_ids list/val_id.txt --save_path sec_seed_s.txt --class_num 21
+PASCAL_DIR=/workspace/hzl/data/VOCdevkit/voc12
+GPU=1
+
+# train step 1 (DSRG training)
+python ../../tools/train.py --solver solver-s.prototxt --weights ../../vgg16_20M_mc.caffemodel --gpu ${GPU}
+python ../../tools/test-ms.py --model models/model-s_iter_8000.caffemodel --images list/train_aug_id.txt --dir ${PASCAL_DIR} --output ${PASCAL_DIR}/DSRGOutput --gpu ${GPU} --smooth true
 
 # train step 2 (retrain)
-python ../../tools/train.py --solver solver-f.prototxt --weights models/model-s_iter_8000.caffemodel --gpu 0
-python ../../tools/test-ms-f.py --model models/model-f_iter_20000.caffemodel --images list/val_id.txt --dir /workspace/hzl/data/VOCdevkit/voc12 --output DSRG_final_output --gpu 0 --smooth true
-python ../../tools/evaluate.py --pred DSRG_final_output --gt /workspace/hzl/data/VOCdevkit/voc12/SegmentationClass --test_ids list/val_id.txt --save_path sec_seed_a_8000.txt --class_num 21
+python ../../tools/train.py --solver solver-f.prototxt --weights models/model-s_iter_8000.caffemodel --gpu ${GPU}
+#python ../../tools/test-ms-f.py --model models/model-f_iter_20000.caffemodel --images list/val_id.txt --dir /workspace/hzl/data/VOCdevkit/voc12 --output DSRG_final_output --gpu 0 --smooth true
 
 # test
-python ../../tools/test-ms-f.py --model models/model-f_iter_20000.caffemodel --images list/test_id.txt --dir /workspace/hzl/wsis-mask/data/voc12 --output DSRG_final_test_output --gpu 1 --smooth true
+python ../../tools/test-ms-f.py --model models/model-f_iter_20000.caffemodel --images list/test_id.txt --dir ${PASCAL_DIR} --output DSRG_final_test_output --gpu ${GPU} --smooth true
+python ../../tools/evaluate.py --pred DSRG_final_test_output --gt ${PASCAL_DIR}/SegmentationClass --test_ids list/test_id.txt --save_path DSRG_result_final.txt --class_num 21
