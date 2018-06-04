e6c5ab17ac79960b12ed3b49273df1f7b0ad6113
Zilong Huang <843133702@qq.com>
2018-06-04 11:38:25 +0800

    Update run-s.sh

--- training/experiment/seed_mc/run-s.sh
+++ training/experiment/seed_mc/run-s.sh
@@ -7,8 +7,5 @@ python ../../tools/test-ms.py --model models/model-s_iter_8000.caffemodel --imag
 
 # train step 2 (retrain)
 python ../../tools/train.py --solver solver-f.prototxt --weights models/model-s_iter_8000.caffemodel --gpu ${GPU}
-#python ../../tools/test-ms-f.py --model models/model-f_iter_20000.caffemodel --images list/val_id.txt --dir /workspace/hzl/data/VOCdevkit/voc12 --output DSRG_final_output --gpu 0 --smooth true
-
-# test
-python ../../tools/test-ms-f.py --model models/model-f_iter_20000.caffemodel --images list/test_id.txt --dir ${PASCAL_DIR} --output DSRG_final_test_output --gpu ${GPU} --smooth true
-python ../../tools/evaluate.py --pred DSRG_final_test_output --gt ${PASCAL_DIR}/SegmentationClass --test_ids list/test_id.txt --save_path DSRG_result_final.txt --class_num 21
+python ../../tools/test-ms-f.py --model models/model-f_iter_20000.caffemodel --images list/val_id.txt --dir /workspace/hzl/data/VOCdevkit/voc12 --output DSRG_final_output --gpu 0 --smooth true
+python ../../tools/evaluate.py --pred DSRG_final_output --gt ${PASCAL_DIR}/SegmentationClass --test_ids list/val_id.txt --save_path DSRG_result_final.txt --class_num 21
